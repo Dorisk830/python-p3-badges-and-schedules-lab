aeffbfe2180c9037f248f62757c7e00ba2304bcd
Doris <doris.kiprop@student.moringaschool.com>
2024-01-18 22:31:58 +0300

    ok

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -24,7 +24,7 @@ def batch_badge_creator(names):
     return [f"Hello, my name is {name}." for name in names]
 
 def assign_rooms(names):
-    return [f"Hello, {name}! You'll be assigned to room {index + 1}!" for index, name in enumerate(speakers)]
+    return [f"Hello, {name}! You'll be assigned to room {index + 1}!" for index, name in enumerate(names)]
 
 
     for badge in badges:
